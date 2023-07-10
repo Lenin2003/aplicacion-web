bb74fac63cc5a5e385cfaa6f28719359fd1f5b8c
Lenin2003 <Lenin2003>
2023-07-10 16:44:48 -0500

    actualizacion repositorio

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC0AZIcfiN7yf7ARKBjS2q-7w0Sl8CBP0E",
-  authDomain: "aplicacion-web-72137.firebaseapp.com",
-  projectId: "aplicacion-web-72137",
-  storageBucket: "aplicacion-web-72137.appspot.com",
-  messagingSenderId: "135386394511",
-  appId: "1:135386394511:web:b97db5dc56b7b7961165fa"
+  apiKey: "AIzaSyDCwetR05Pv-GVJJNaHZ_mVCBUDNwUpw9g",
+  authDomain: "aplicacion-web-36cae.firebaseapp.com",
+  projectId: "aplicacion-web-36cae",
+  storageBucket: "aplicacion-web-36cae.appspot.com",
+  messagingSenderId: "139458578189",
+  appId: "1:139458578189:web:135313d7cc11502a249244"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+export {auth, db}
+export default app;
